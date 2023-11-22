ba017f150fd463881f27bbe19d390faa12282730
IgnatVolk <142455386+IgnatVolk@users.noreply.github.com>
2023-11-22 22:03:53 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,33 +1,59 @@
-function main() {
-    const data = [];
-function generateRandomData() {
-        console.log("Random Number: " + item);
+    return data
+    data = generate_random_data()
+def main():
+
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+if __name__ == "__main__":
+import random
+    for item in data:
+
+        print(f"Random Number: {item}")
+    main()
+
+
+def main():
+
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+
+def generate_random_data():
+import random
+        print(f"Random Number: {item}")
+    for item in data:
+
+if __name__ == "__main__":
+    return data        console.log("Random Number: " + item);
+    data.forEach(item => {
+    }
+    });
 }
+
     return data;
 main();
-
-        data.push(Math.floor(Math.random() * 100) + 1);
-
-    });
-    const data = generateRandomData();
-}
-    }
-    data.forEach(item => {
-
-    for (let i = 0; i < 10; i++) {    return data;
-        data.push(Math.floor(Math.random() * 100) + 1);
-    const data = generateRandomData();
-    data.forEach(item => {
 function main() {
     const data = [];
-
-function generateRandomData() {
-    });
-
 }
-
-    }
-        console.log("Random Number: " + item);
-main();
     for (let i = 0; i < 10; i++) {
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+function generateRandomData() {
+
+    const data = generateRandomData();function generateRandomData() {
+    }
+function main() {
+        console.log("Random Number: " + item);
+    const data = generateRandomData();
+    const data = [];
+
 }
+
+main();
+
+    for (let i = 0; i < 10; i++) {
+    });
+    return data;
+    data.forEach(item => {
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
